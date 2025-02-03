ecc9dca5181df7c892990f7797889ee540baa95a
zaamaulan <zaamaulan@gmail.com>
2025-02-03 20:45:06 +0700

    fix eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
 import FilterCategory from "@/components/post/filter-category/filter-category"
 import { PostList } from "@/components/post/post-list/post-list"
 import { SearchPost } from "@/components/post/search-post/search-post"
+import { Suspense } from "react"
 
 const HomePage = () => {
   return (
     <div className="flex flex-col gap-10 w-full">
       <div className="flex items-center gap-2 justify-between">
-        <SearchPost />
-        <FilterCategory />
+        <Suspense>
+          <SearchPost />
+          <FilterCategory />
+        </Suspense>
       </div>
-      <PostList />
+      <Suspense>
+        <PostList />
+      </Suspense>
     </div>
   )
 }
